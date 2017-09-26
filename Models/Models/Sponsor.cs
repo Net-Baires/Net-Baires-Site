cafdf91effaae7a2b06011ee0014ae267831c20a
eyanez89 <eyanez.89@gmail.com>
2017-09-26 18:18:37 -0300

    Attr adition

--- Models/Models/Sponsor.cs
+++ Models/Models/Sponsor.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using JsonApiDotNetCore.Models;
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
     public class Sponsor : IEntity
     {
+        [Attr("Id")]
         public int Id { get; set; }
 
+        [Attr("Name")]
         public string Name { get; set; }
 
+        [Attr("PhotoURL")]
         public string PhotoURL { get; set; }
 
+        [Attr("URL")]
         public string URL { get; set; }
 
         [NotMapped]
